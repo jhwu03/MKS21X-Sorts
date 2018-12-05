8f391f592685d1b3e7f60417dc04f41ff374f569
jwu03 <jwu03@stuy.edu>
2018-12-04 21:39:03 -0500

    tested with a little main

--- sort.java
+++ sort.java
@@ -4,17 +4,24 @@ public class sort{
 //choose the next smallest put it into ary[1]  (you can ignore the slots before the current position)
 //choose the next smallest put it into ary[2]  (you can ignore the slots before the current position)
   for(int i = 0; i < ary.length;i++){
-    int smallest = ary[i];
-    int current = i;
-  for(int n = current; n < ary.length;n++){
-    if(ary[n] <= smallest){
-      current = n;
+    int smallest = i;
+  for(int n = smallest; n < ary.length;n++){
+    if(ary[n] <= ary[smallest]){
+      smallest = n;
     }
   }
-  int temp = ary[current];
-  ary[i] = temp;
-  ary[current] = smallest;
+  int temp = ary[i];
+  ary[i] = ary[smallest];
+  ary[smallest] = temp;
   }
+  }
+
+  public static void main(String[] args){
+    int[] ary = {15, 12, 6, 7, 14, 2, 1, 3, 17};
+    selectionsort(ary);
+    for (int i = 0; i < ary.length;i++){
+      System.out.println(ary[i]);
+    }
   }
 
 }
